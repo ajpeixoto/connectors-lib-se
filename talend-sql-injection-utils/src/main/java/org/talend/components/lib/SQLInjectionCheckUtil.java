817be9849c15e8b2fa6ac26903ffa8707600c359
pyzhou <pyzhou@talend.com>
2023-03-30 15:50:07 +0800

    fix(TDI-49313): fix NPE on sql injection util (#43)

--- talend-sql-injection-utils/src/main/java/org/talend/components/lib/SQLInjectionCheckUtil.java
+++ talend-sql-injection-utils/src/main/java/org/talend/components/lib/SQLInjectionCheckUtil.java
@@ -7,14 +7,17 @@ public class SQLInjectionCheckUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SQLInjectionCheckUtil.class);
 
-    public static boolean checkSQLInjection(String identifier){
-        if(identifier.length() > 255){
+    public static boolean checkSQLInjection(String identifier) {
+        if (identifier == null) {
+            return false;
+        }
+        if (identifier.length() > 255) {
             return true;
         }
-        String[] riskyChar = {"\"",";","'","--","/*","xp_","="," "};
+        String[] riskyChar = {"\"", ";", "'", "--", "/*", "xp_", "=", " "};
         for (String s : riskyChar) {
-            if(identifier.contains(s)){
-                LOGGER.warn("Potential SQL Injection risks: {}",identifier);
+            if (identifier.contains(s)) {
+                LOGGER.warn("Potential SQL Injection risks: {}", identifier);
                 return true;
             }
         }
